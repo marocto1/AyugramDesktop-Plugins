4af3eb3e57505c783d2f2d733fdd8dbf8d036b5d
marlon <maroctokrutoy1488@gmail.com>
2026-08-30 03:17:05 +0500

    feat: expose Android SDK compatibility version

---
+++ runtime/sdk/_sdk_version.py
@@ -0,0 +1,10 @@
+"""SDK compatibility version advertised to existing plugins."""
+
+version = (1, 4, 0)
+version_str = "1.4.0"
+__version__ = version_str
+beta = False
+__beta__ = False
+
+# Version of this Desktop compatibility implementation itself.
+desktop_version = "0.1.0-dev"
